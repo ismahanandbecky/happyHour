627ff90f88f5b34cbae640d4a7883e145a82c9ab
rebrobinson <rebrobinson>
2019-05-24 11:33:56 -0400

    saving variables for function call

--- trying.js
+++ trying.js
@@ -18,7 +18,6 @@ cocktailApp.getDrinks = function (userChoice) {
     }).then(function (results) {
         // calling the chooseDrinks function and passing the argument results.drinks
         cocktailApp.chooseDrinks(results.drinks);
-
     })
 }
 // defining the chooseDrinks function that will take the array drinksArray as an argument. 
@@ -26,21 +25,55 @@ cocktailApp.chooseDrinks = function (drinksArray) {
     console.log(drinksArray);
 }
 
+//on click of the thirsty button, do the following
+$('.thirsty').on('click', function (event) {
+    event.preventDefault();
+    //this is so that the id doesn't keep appending to the URL every time you click the button
+    const location = window.location.href.split('#')[0];
+    //this is appending the #questionPage id to the url of where we are so that it takes us there
+    window.location = `${location}#questionPage`;
+})
+
 // on click of the makeMe button do the following:
 $('.makeMe').on('click', function (event) {
     // on click of the button prevent the native behaviour of the browser, to refresh when a button is clicked. 
     event.preventDefault();
-    //  on selection of radio button store the user selection as a variable called userSelection.
+
+    //this is so that the id doesn't keep appending to the URL every time you click the button
+    const location = window.location.href.split('#')[0];
+    //this is appending the #resultPage id to the url of where we are so that it takes us there
+    window.location = `${location}#resultPage`;
+
+    //on selection of radio button store the user selection as a variable called userSelection.
     const userSelection = $('input[name=selection]:checked').val();
 
     console.log(userSelection)
+
+    // if (userSelection == 'alcoholic') {
+    //     cocktailApp.getDrinks(`Alcoholic`);
+    //     // console.log('worked');
+    // } else {
+    //     cocktailApp.getDrinks(`Non_Alcoholic`);
+    //     // console.log('it worked');
+    // }
+
+    if (userSelection == 'alcoholic') {
+        const alcDrinksArray = cocktailApp.getDrinks(`Alcoholic`);
+        // console.log('worked');
+    } else {
+        const nonAlcDrinksArray = cocktailApp.getDrinks(`Non_Alcoholic`);
+        // console.log('it worked');
+    }
+
 })
+
+
 
 
 
 // make sure the document is ready
 $(function () {
-    cocktailApp.getDrinks(`Alcoholic`);
+    // cocktailApp.getDrinks(`Alcoholic`);
     // cocktailApp.getDrinks(`Non_Alcoholic`);
 
 });
